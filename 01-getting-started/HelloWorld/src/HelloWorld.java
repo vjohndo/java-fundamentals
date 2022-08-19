8a3a44c9926ba3582f0ba2f35a085c73a21afa72
vjohndo <v.john.do@outlook.com>
2022-08-19 16:58:54 +1000

    lesson 2.23 - getters and setters

--- 01-getting-started/HelloWorld/src/HelloWorld.java
+++ 01-getting-started/HelloWorld/src/HelloWorld.java
@@ -1,5 +1,12 @@
+import model.Person;
+import java.time.LocalDate;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Person johnny = new Person("johnny", "smith", LocalDate.of(200, 11, 1));
+        // Could allow amy to create Person.
+
+        System.out.format("The person's first name is: %s.", johnny.getFirstName());
     }
 }
--- 01-getting-started/HelloWorld/src/model/Person.java
+++ 01-getting-started/HelloWorld/src/model/Person.java
@@ -3,11 +3,18 @@ package model;
 import java.time.LocalDate;
 
 public class Person {
+    // These things are called fields
     private String firstName;
     private String lastName;
     private LocalDate dob;
     private Address address;
 
+    // No arg constructor... some frameworks require one
+    private Person() {
+
+    }
+
+    // Allowing methods to be passed in
     public Person(String firstName, String lastName, LocalDate dob) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -15,7 +22,7 @@ public class Person {
     }
 
     public String getFirstName() {
-        return firstName;
+        return firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
     }
 
     public void setFirstName(String firstName) {
