b68c8ea09f115b39148c14e78c234fa94805285b
vjohndo <v.john.do@outlook.com>
2022-08-19 23:04:41 +1000

    lesson 2.27 complete - isBlank isEmpty

---
+++ 03-working-with-text/People/src/BlankEmptyStrings.java
@@ -0,0 +1,7 @@
+public class BlankEmptyStrings {
+    public static void main(String[] args) {
+        String myText = "";
+        System.out.println(myText.isEmpty());
+        System.out.println(myText.isBlank());
+    }
+}
