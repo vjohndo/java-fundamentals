935cd143a78eee0fbb45c99f7aac4099fe29f5c5
vjohndo <v.john.do@outlook.com>
2022-08-19 23:26:31 +1000

    completed lesson 2.30 - charAt

---
+++ 03-working-with-text/People/src/CharAtStrings.java
@@ -0,0 +1,8 @@
+public class CharAtStrings {
+    public static void main(String[] args) {
+        String myText = "Apples";
+        System.out.println(myText.charAt(3));
+        //  System.out.println(myText.charAt(9)); ==> this will blow up the program
+        
+    }
+}
