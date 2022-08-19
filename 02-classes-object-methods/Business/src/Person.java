cf0f004a728da466c8f67d55bd0e4282ec34139d
vjohndo <v.john.do@outlook.com>
2022-08-19 14:31:09 +1000

    lesson 2.13 - methods

--- 02-classes-object-methods/Business/src/Person.java
+++ 02-classes-object-methods/Business/src/Person.java
@@ -1,14 +1,30 @@
 import business.Company;
 
 public class Person {
-    private String firstName; // can initialise it e.g. private String firstName = "John";
-    private int age;
-    private long id;
-    private char middleInitial;
-    private Company company = new Company();
-    private NewsAgency agency;
+    private String middleName = "Christopher";
 
+
+    public void sayHello() {
+        System.out.println("hello");
+    }
+
+    public void saySomething(String message) {
+        System.out.println(message);
+    }
+
+    public char getMiddleInitial() {
+        return middleName.charAt(0);
+    }
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
     public static void main(String[] args) {
-        System.out.println();
+        Person p1 = new Person();
+        p1.sayHello();
+        p1.saySomething("My groovy message");
+        char middleInitial = p1.getMiddleInitial();
+        System.out.println(middleInitial);
+        System.out.println(p1.add(5,5));
     }
 }
