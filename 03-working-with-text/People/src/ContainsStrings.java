7afa994cd4564e7a12c2e3098e34db29d8a0413f
vjohndo <v.john.do@outlook.com>
2022-08-19 23:39:03 +1000

    completed lesson 2.32 - contains

---
+++ 03-working-with-text/People/src/ContainsStrings.java
@@ -0,0 +1,6 @@
+public class ContainsStrings {
+    public static void main(String[] args) {
+        String myText = "Four score and seven years ago";
+        System.out.println(myText.contains("seven"));
+    }
+}
