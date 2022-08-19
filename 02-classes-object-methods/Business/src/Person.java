c8efc77f0587d0e9eb01855c1cea5f4783f9a39a
vjohndo <v.john.do@outlook.com>
2022-08-19 14:38:46 +1000

    lesson 2.14 - varargs

--- 02-classes-object-methods/Business/src/Person.java
+++ 02-classes-object-methods/Business/src/Person.java
@@ -19,6 +19,14 @@ public class Person {
     public int add(int num1, int num2) {
         return num1 + num2;
     }
+
+    public void testArrayMethods(String[] words) {
+        // do something clever with words
+    }
+
+    public void testVarargs(int num1, int num2, String... words) {
+        // do something clever with words
+    }
     public static void main(String[] args) {
         Person p1 = new Person();
         p1.sayHello();
@@ -26,5 +34,7 @@ public class Person {
         char middleInitial = p1.getMiddleInitial();
         System.out.println(middleInitial);
         System.out.println(p1.add(5,5));
+        p1.testArrayMethods(new String[] {"1", "2", "3"});
+        p1.testVarargs(3, 5, "one", "two", "three");
     }
 }
