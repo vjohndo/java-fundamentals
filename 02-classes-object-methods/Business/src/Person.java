3e0272693dc5c26c43c0b41388defb7b274c0b08
vjohndo <v.john.do@outlook.com>
2022-08-19 15:31:31 +1000

    lesson 2.18 - object superclass

--- 02-classes-object-methods/Business/src/Person.java
+++ 02-classes-object-methods/Business/src/Person.java
@@ -3,6 +3,29 @@ import business.Company;
 public class Person {
     private String middleName = "Christopher";
     public static double PI = 3.14;
+    public static int[] nums;
+
+    // static initializers
+    static {
+        nums = new int[5];
+        nums[0] = 3;
+        nums[1] = 3;
+        nums[2] = 3;
+        nums[3] = 3;
+        nums[4] = 3;
+    }
+
+    // static method allows to define more complicated static variable.
+    // need to call initNums on the variable
+    public static int[] initNums() {
+        int[] nums = new int[5];
+        nums[0] = 3;
+        nums[1] = 3;
+        nums[2] = 3;
+        nums[3] = 3;
+        nums[4] = 3;
+        return nums;
+    }
 
 
     public void sayHello() {
@@ -38,5 +61,11 @@ public class Person {
         p1.testArrayMethods(new String[] {"1", "2", "3"});
         p1.testVarargs(3, 5, "one", "two", "three");
         System.out.println(PI);
+    }
+
+    // Default class is Object which contains the toString method
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
